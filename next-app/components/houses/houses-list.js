ad97b098363af6e55f37d18945612a119f3e4c71
AlexandraSeuntjens <madlog2912@gmail.com>
2023-05-03 17:11:04 +0200

    woensdag

--- next-app/components/houses/houses-list.js
+++ next-app/components/houses/houses-list.js
@@ -3,19 +3,20 @@ import HouseItem from "./houses-item";
 function HouseList(props) {
   const { items } = props;
 
-  return;
-  
-    {items.map((event) => (
-      <HouseItem
-        key={house.id}
-        id={house.id}
-        title={house.title}
-        description={house.description}
-        location={house.location}
-        image={house.image}
-      />
-    ))}
-
+  return (
+    <ul>
+      {items.map((event) => (
+        <HouseItem
+          key={house.id}
+          id={house.id}
+          title={house.title}
+          description={house.description}
+          location={house.location}
+          image={house.image}
+        />
+      ))}
+    </ul>
+  );
 }
-
 export default HouseList;
+
--- next-app/pages/index.js
+++ next-app/pages/index.js
@@ -1,9 +1,11 @@
-import { getFeaturedEvents } from "@/dummy-data";
-import classes from "./homepage.module.css";
-import HouseList from "@/components/houses/houses-list";
+import { getFeaturedEvents } from "../dummy-data";
+import HouseList from "../components/houses/houses-list";
 const featuredHouses = getFeaturedEvents();
 
 function HomePage() {
+  const featuredHouses = getFeaturedEvents;
+
+
   return (
     <div>
       <HouseList items={featuredHouses}/>
